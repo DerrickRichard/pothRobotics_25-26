579bf7a8eb986d3c5339d90a687c04183f98bd4e
Derrick Richard <derrick.richard7@proton.me>
2025-11-13 07:15:22 -0600

    Create chicken.java

---
+++ chicken.java
@@ -0,0 +1,9 @@
+//Autonomous to drive 4 inches out of the launch pad.
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
